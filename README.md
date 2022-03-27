36ad7fb38edf53ffa3fca4b36fb7dceb8cfb4cbd
Jim <jim.hinson@live.com>
2022-03-26 20:01:11 -0400

    Getting started with remix

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # myRemix
+To Do:  
+* 
+
+Notes:  
+from c:\src\my-remix:  
+npm run dev  
+- or -  
+npm start
+
+Reference:  
+https://developer.mozilla.org/en-US/  
+https://www.prisma.io/  
+
+Look at:  
+https://fly.io/  
--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {Scripts} from 'remix'
-
+import {Link } from "remix";
 function App() {
   const [count, setCount] = React.useState(0)
   return (
@@ -11,7 +11,13 @@ function App() {
       <body>
         <p>This is a remix app. Hooray!</p>
         <button onClick={() => setCount(c => c + 1)}>{count}</button>
+<br></br>
+<br></br>
+
         <Scripts></Scripts>
+        <Link to="/posts"><b>Posts</b></Link>
+{/* hi */}
+
       </body>
     </html>
   )
