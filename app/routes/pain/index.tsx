9017d5c339a765df6c3657bcfede8fa00e9bf1bb
Jim <jim.hinson@live.com>
2022-09-05 19:32:00 -0400

    Planning pain object

--- app/routes/pain/index.tsx
+++ app/routes/pain/index.tsx
@@ -1,7 +1,20 @@
+
+import { Link, useLoaderData } from "remix";
+/**
+ * Data to capture
+ * location
+ * Severity
+ * 
+ * @returns 
+ */
+
 export default function Pokemons() {
     return (
       <div>
         <h2>Pain</h2>
+        <h3>It's worse on rainy days</h3>
+        <href 
+        <Link to={"https://www.google.com"}>{"Go to google"}</Link>
       </div>
     );
   }
